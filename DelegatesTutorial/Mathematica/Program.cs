e64e47e90651b798d7831a452c4347e595f56c5e
Vadim Huschin <vadymhuschin@gmail.com>
2025-05-20 18:16:16 +0200

    delegate version added

--- DelegatesTutorial/Mathematica/Program.cs
+++ DelegatesTutorial/Mathematica/Program.cs
@@ -1,21 +1,41 @@
-﻿Console.WriteLine("Hello, World");
+﻿
+var intagrator = new Integrator();
+
+Console.WriteLine(intagrator.Integrate(0, 1, 0.01, new Squarer()));
+Console.WriteLine(intagrator.Integrate(0, 1, 0.01, new Sinuser()));
 
 public class Integrator
 {
-    public double Integrate()
+    public double Integrate(double start, double end, double step, IIntegrable function)
     {
-        var start = 0.0;
-        var end = 1.0;
-        var step = 0.1;
-
         var sum = 0.0;
 
         for (var x = start; x < end; x += step)
         {
-            var y = x * x;
+            var y = function.Calculate(x);
             sum += y * step;
         }
 
         return sum;
+    }
+}
+
+public interface IIntegrable
+{
+    double Calculate(double x);
+}
+
+public class Squarer : IIntegrable
+{
+    public double Calculate(double x)
+    {
+        return x * x;
+    }
+}
+public class Sinuser : IIntegrable
+{
+    public double Calculate(double x)
+    {
+        return Math.Sin(x);
     }
 }
---
+++ DelegatesTutorial/MathematicaDelegete/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var intagrator = new Integrator();
+
+Console.WriteLine(intagrator.Integrate(0, 1, 0.01, x => Math.Sin(x)));
+Console.WriteLine(intagrator.Integrate(0, 1, 0.01, x => x*x));
+
+void OutputMessage(string message)
+{
+    Console.WriteLine(message);
+}
+public class Integrator
+{
+    public double Integrate(double start, double end, double step, Func<double, double> function)
+    {
+        var sum = 0.0;
+
+        for (var x = start; x < end; x += step)
+        {
+            var y = function(x);
+            sum += y * step;
+        }
+
+        return sum;
+    }
+}
